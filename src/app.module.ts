a20bc5ac089d70b43ac143af5ca9d04c0468cdcd
tarakj <tarak.cdtp@gmail.com>
2025-06-19 22:23:46 +0100

    crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
+import { CarModule } from './modules/car/car.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, CarModule],
   controllers: [AppController],
   providers: [AppService],
 })
